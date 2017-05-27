5dabf70a4bceeb9c55f2d255a292107417f7bbb6
William Saunders <william@williamsaunders.net>
2017-05-27 17:47:38 -0400

    Avoid accidentally launching multiple copies of hyper

--- move_window.sh
+++ move_window.sh
@@ -1,6 +1,8 @@
 #!/bin/zsh
 # ZSH script to move windows to designated parts of the screen, activated by keyboard shortcuts
-
+set -x
+if [[ $(pgrep -c -f "move_window.sh") -gt 1 ]] ; then ; exit 0 ; fi
+# Do stuff
 xdotool getdisplaygeometry | read x y; echo "$x $y"
 newy=0
 # Leave room for panel at the bottom of the screen
@@ -10,6 +12,7 @@ case $1 in
   center)
     newx=$((1 * $x / 6))
     xsize=$((4 * $x / 6))
+
   ;;
   left)
     newx=0
@@ -29,16 +32,22 @@ case $1 in
     xsize=$((4 * $x / 6))
     newy=$((4 * $y / 6))
     ysize=$((2 * $y / 6 - 25))
-    if [[ "$(xdotool search --desktop 0 --class Hyper)" == "$(xdotool getactivewindow)" ]] ; then
-      xdotool search --desktop 0 --class Hyper windowminimize
-    else
-      xdotool search --desktop 0 --class Hyper windowsize ${xsize} ${ysize} windowmove ${newx} ${newy} windowactivate
-      if [[ $? != 0 ]] ; then
-        /opt/Hyper/hyper > ~/hyper.log 2>&1
-        xdotool search --sync --desktop 0 --class Hyper windowsize ${xsize} ${ysize} windowmove ${newx} ${newy} windowactivate
+    hyper_ids=($(xdotool search --class Hyper))
+    active_window=$(xdotool getactivewindow)
+    if [[ -z "${hyper_ids}" ]]; then
+      sleep 1
+      hyper_ids=($(xdotool search --class Hyper))
+      if [[ -z "${hyper_ids}" ]]; then
+        if [[ $(pgrep -c -f "move_window.sh") -gt 1 ]] ; then ; exit 0 ; fi
+        /opt/Hyper/hyper > ~/hyper.log 2>&1 &
+        xdotool search --sync --class Hyper windowsize ${xsize} ${ysize} windowmove ${newx} ${newy} windowactivate
       fi
+    elif [[ ${hyper_ids[(i)${active_window}]} -le ${#hyper_ids} ]] ; then
+      xdotool windowminimize ${active_window}
+    else
+      xdotool search --class Hyper windowsize ${xsize} ${ysize} windowmove ${newx} ${newy} windowactivate
     fi
-    exit
+    exit 0
   ;;
 esac
 xdotool getactivewindow windowsize "${xsize}" "${ysize}" windowmove "${newx}" "${newy}"
